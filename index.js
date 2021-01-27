64a1c93ba4be133d955f6c59117ca5cdf419773e
Angel Baro <angelbaro44@gmail.com>
2021-01-26 20:35:34 -0500

    Added alt solution

--- index.js
+++ index.js
@@ -51,3 +51,41 @@ function calculateDamage(yourType, opponentType, attack, defense){
 
 
 
+
+
+//alt Solutions using an object
+
+//     function calculateDamage(yourType, opponentType, attack, defense) {
+//       'use strict';
+      
+//       let damageEff = {
+//         fire: {
+//           fire: 0.5,
+//           grass: 2,
+//           water: 0.5,
+//           electric: 1
+//         },
+//         water: {
+//           grass: 0.5,
+//           water: 0.5,
+//           electric: 0.5,
+//           fire: 2
+//         },
+//         grass: {
+//           electric: 1,
+//           water: 2,
+//           fire: 0.5,
+//           grass: 0.5
+//         },
+//         electric: {
+//           grass: 1,
+//           electric: 0.5,
+//           water: 2,
+//           fire: 1
+//         }
+//       };
+      
+//       let damage = Math.ceil(50 * (attack / defense) * damageEff[yourType][opponentType]);
+      
+//       return damage;
+//     }
